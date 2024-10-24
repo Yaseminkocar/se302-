7c761f9660397f6c7da3fa6397fa994f50223f74
Betül Sinem Çetiner <120674441+bsinemcetiner@users.noreply.github.com>
2024-10-24 19:29:34 +0300

    committtt

--- src/Test.java
+++ src/Test.java
@@ -9,7 +9,5 @@ public class Test {
         System.out.println("deneme");
         System.out.println("aaaaaaaaaaaaaaaaaaaa");
         System.out.println("yaso istediği için deneme");
-        System.out.println("aaaaaaaaaaaa");
     }
-
 }
