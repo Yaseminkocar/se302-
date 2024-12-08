b2bca6a7277f9e0ee0cd0bcff629918beca346ce
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2024-12-08 16:08:43 +0300

    yasemin's commit

--- src/Student.java
+++ src/Student.java
@@ -11,6 +11,38 @@ public class Student {
         this.assignedClass = assignedClass;
         this.details = details;
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAssignedClass() {
+        return assignedClass;
+    }
+
+    public void setAssignedClass(String assignedClass) {
+        this.assignedClass = assignedClass;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
 
 
 }
