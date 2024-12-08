e81aeef0c613b6e2b7e3302da966f0436e5945a9
Betül Sinem Çetiner <120674441+bsinemcetiner@users.noreply.github.com>
2024-12-08 15:59:42 +0300

    Student class added

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    private String name;
+    private String id;
+    private String assignedClass;
+    private String details;
+
+
+    public Student(String name, String id, String assignedClass, String details) {
+        this.name = name;
+        this.id = id;
+        this.assignedClass = assignedClass;
+        this.details = details;
+    }
+}
